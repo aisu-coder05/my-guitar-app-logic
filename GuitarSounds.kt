e7ad0bb015cddb8cfbf21e420b1829306d011409
aisu-coder05 <aishwariyapar02@gmail.com>
2026-07-20 12:06:15 +0530

    Add GuitarSounds.kt for chord sound logic
    This file handles the logic for playing guitar chords.

---
+++ GuitarSounds.kt
@@ -0,0 +1,12 @@
+// This file handles all the guitar chord sound logic
+fun playGChord() {
+    println("🔊 [SOUND] Playing G Major: Tinggg~ 🎸")
+}
+
+fun playCChord() {
+    println("🔊 [SOUND] Playing C Major: Tanggg~ 🎸")
+}
+
+fun playAmChord() {
+    println("🔊 [SOUND] Playing A Minor: Tunggg~ 🎸")
+}
